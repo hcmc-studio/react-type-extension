4b6e6ba5ea14e685ed6c8bdec365ed252567610d
Ji-Hwan KIM <nodealdev@gmail.com>
2023-09-06 16:46:08 +0900

    [Script push]

---
+++ src/index.ts
@@ -0,0 +1,77 @@
+import {Dispatch, useState} from "react";
+
+export type StatePair<T> = {
+    value: T
+    setter: Dispatch<T>
+}
+
+export type BooleanStatePair = StatePair<boolean> & {
+    setTrue(): void
+    setFalse(): void
+    toggle(): void
+}
+
+export type UndefinedStatePair<T> = StatePair<T | undefined> & {
+    unset(): void
+}
+
+export type ErrorStatePairs = {
+    title: StatePair<string>
+    body: StatePair<any>
+    closeable: BooleanStatePair
+    visible: BooleanStatePair
+    show(title: string, body: any, closeable?: boolean): void
+}
+
+export function useStatePair<T>(defaultValue: T): StatePair<T> {
+    const [value, setter] = useState<T>(defaultValue)
+
+    return { value, setter }
+}
+
+export function useBooleanPair(defaultValue: boolean = false): BooleanStatePair {
+    const [value, setter] = useState<boolean>(defaultValue)
+
+    return {
+        value,
+        setter,
+        setTrue() {
+            setter(true)
+        },
+        setFalse() {
+            setter(false)
+        },
+        toggle() {
+            setter(!value)
+        }
+    }
+}
+
+export function useUndefinedPair<T>(defaultValue: T | undefined = undefined): UndefinedStatePair<T> {
+    const [value, setter] = useState<T | undefined>(defaultValue)
+
+    return {
+        value,
+        setter,
+        unset() {
+            setter(undefined);
+        }
+    }
+}
+
+export function useErrorPairs(): ErrorStatePairs {
+    const states: ErrorStatePairs = {
+        title: useStatePair(''),
+        body: useStatePair({}),
+        closeable: useBooleanPair(true),
+        visible: useBooleanPair(false),
+        show(title: string, body: any, closeable: boolean = true) {
+            states.title.setter(title)
+            states.body.setter(body)
+            states.closeable.setter(closeable)
+            states.visible.setTrue()
+        }
+    }
+
+    return states
+}
