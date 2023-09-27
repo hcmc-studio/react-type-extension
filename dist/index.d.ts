e59c06360c6a2c3aa9b45d666787549b30fc5f72
Ji-Hwan KIM <nodealdev@gmail.com>
2023-09-27 23:40:51 +0900

    [Script push]

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
-import { Dispatch } from "react";
+import { Dispatch, JSX } from "react";
+import { ListOptionFilter, ListOptionOrder, SortOrder } from "ts-protocol-extension";
 export type StatePair<T> = {
     value: T;
     setter: Dispatch<T>;
@@ -28,3 +29,25 @@ export type ErrorPairsConfig = {
     visible?: boolean;
 };
 export declare function useErrorPairs(config?: ErrorPairsConfig): ErrorStatePairs;
+export type ListOptionFilterStatePairs<Filter extends ListOptionFilter> = {
+    [key in keyof Filter]: StatePair<Filter[key]>;
+};
+export type ListOptionFilterIndicatorProps<Filter extends ListOptionFilter, Props> = Props & {
+    statePairs: ListOptionFilterStatePairs<Filter>;
+};
+export type ListOptionFilterSelectorProps<Filter extends ListOptionFilter, Props> = Props & {
+    selectedKey: keyof Filter;
+    statePairs: ListOptionFilterStatePairs<Filter>;
+    onDoneClick?: () => void;
+};
+export interface ListOptionFilterExtension<Filter extends ListOptionFilter, IndicatorProps, SelectorProps> {
+    names: Record<keyof Filter, string>;
+    useStatePairs(initial?: Partial<Filter>): ListOptionFilterStatePairs<Filter>;
+    indicators(props: ListOptionFilterIndicatorProps<Filter, IndicatorProps>): JSX.Element;
+    selectors(props: ListOptionFilterSelectorProps<Filter, SelectorProps>): JSX.Element;
+}
+export type ListOptionOrderStatePairs<Order extends ListOptionOrder> = Record<keyof Order, UndefinedStatePair<SortOrder>>;
+export interface ListOptionOrderExtension<Order extends ListOptionOrder> {
+    names: Record<keyof Order, string>;
+    useStatePairs(initial?: Partial<Order>): ListOptionOrderStatePairs<Order>;
+}
